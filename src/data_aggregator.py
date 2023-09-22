1438a6a7d3811765a224aa2d5f88c3f67c5c4490
Matteo Periani <matteo.periani2@studio.unibo.it>
2023-09-22 10:45:36 +0200

    fixes

--- src/data_aggregator.py
+++ src/data_aggregator.py
@@ -12,7 +12,7 @@ classFields = ['whoTarget', 'intentYN', 'sexYN','offensiveYN']
 
 for split in ["train", "validation", "test"]:
     df = pd.read_csv(os.path.join(os.getcwd(), data_folder, raw_folder, f"{split}.csv"))
-    aggDict = {c: lambda x: sorted(filter(lambda x: x, set(x))) for c in textFields}
+    aggDict = {c: lambda x: sorted(filter(lambda x: x, list(x))) for c in textFields}
 
     aggDict.update({c: lambda x: np.mean(x) for c in classFields})
     df[textFields] = df[textFields].fillna("")
@@ -21,6 +21,8 @@ for split in ["train", "validation", "test"]:
     agg_df["hasBiasedImplication"] = (agg_df["targetStereotype"].apply(len) == 0).astype(int)
     
     agg_df['targetMinority'] = agg_df['targetMinority'].apply(lambda c: dict(Counter(c)))
-    agg_df['targetMinority'] = agg_df['targetMinority'].apply(lambda c: {k: v / total for total in (sum(c.values(), 0.0),) for k, v in c.items()})
+    agg_df['targetMinority'] = agg_df['targetMinority'].apply(lambda c: {k: round(v / total,2) for total in (sum(c.values(), 0.0),) for k, v in c.items()})
 
     agg_df.to_pickle(os.path.join(os.getcwd(), data_folder, aggregate_folder, f"{split}.pkl"))
+
+    
