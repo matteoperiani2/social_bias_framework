91b36c0225d7e2459b93db734dff1675690794e8
Simone999 <simonemele999@gmail.com>
2023-11-28 20:38:00 +0100

    move configuration to file

--- src/config.py
+++ src/config.py
@@ -1,117 +1,25 @@
 import os
-import torch
+from typing import Optional
 
-class PropertyDict(dict):
-    def __getattr__(self, key):
-        if key in self:
-            return self[key]
-        raise AttributeError(
-            f"'{self.__class__.__name__}' object has no attribute '{key}'"
-        )
+from omegaconf import OmegaConf
 
-    def __setattr__(self, key, value):
-        self[key] = value
 
 class Config:
-    
-    class Dataset:
-        
-        data_dir: str = "data"
-        
-        raw_dir: str = os.path.join(data_dir, "raw")
-        train_data_raw: str = os.path.join(raw_dir, "train.csv")
-        val_data_raw: str = os.path.join(raw_dir, "validation.csv")
-        test_data_raw: str = os.path.join(raw_dir, "test.csv")
+    class Model:
+        gpt2 = "gpt2"
 
-        preproc_dir: str = os.path.join(data_dir, "preproc")
-        train_data_preproc: str = os.path.join(preproc_dir, "train.pkl")
-        val_data_preproc:str = os.path.join(preproc_dir, "validation.pkl")
-        test_data_preproc: str = os.path.join(preproc_dir, "test.pkl")
+    model = Model()
 
-        aggregated_dir: str = os.path.join(data_dir, "aggregated")
-        train_data_agg: str = os.path.join(aggregated_dir, "train.pkl")
-        val_data_agg: str = os.path.join(aggregated_dir, "validation.pkl")
-        test_data_agg: str = os.path.join(aggregated_dir, "test.pkl")
+    @staticmethod
+    def load_config(
+        config_path="config", config_name="main", model_name: Optional[str] = None
+    ):
+        config_file = os.path.join(config_path, config_name) + ".yaml"
+        config = OmegaConf.load(config_file)
 
-        train_dir: str = os.path.join(data_dir, "train")
-        train_data: str = os.path.join(train_dir, "train.pkl")
-        val_data: str = os.path.join(train_dir, "validation.pkl")
-        test_data: str = os.path.join(train_dir, "test.pkl")
+        if model_name is not None:
+            model_file = os.path.join(config_path, "model", model_name) + ".yaml"
+            model_config = OmegaConf.load(model_file)
+            config.merge_with({"model": model_config})
 
-    class ModelParameters:
-        model_name:str = "distilgpt2"
-        checkpoint_name:str = "distilgpt2"
-        epochs:int = 3
-        batch_size:int = 2
-        lr:int = 1e-5
-        padding_side:str = "left"
-        device:str = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-        special_tokens = {
-            "pad_token": "<|pad|>",
-            "sep_token": "<|sep|>",
-            "additional_special_tokens": [
-                "<|offY|>",
-                "<|offN|>",
-                "<|sexY|>",
-                "<|sexN|>",
-                "<|intY|>",
-                "<|intN|>",
-                "<|grpY|>",
-                "<|grpN|>",
-                "<|ingrpN|>",
-                "<|ingrpY|>",
-            ],
-        }
-
-    class Utils:
-        label_encoder = {
-            0: {0: "<|offN|>", 1: "<|offY|>"},
-            1: {0: "<|intN|>", 1: "<|intY|>"},
-            2: {0: "<|sexN|>", 1: "<|sexY|>"},
-            3: {0: "<|grpN|>", 1: "<|grpY|>", 2:"<|pad|>"},    
-            4: {0: "<|ingrpN|>", 1: "<|ingrpY|>", 2:"<|pad|>"},
-        }
-
-    class Checkpoints:
-        def __init__(
-            self, gpt2="gpt2", distilgpt2="distilgpt2"
-        ) -> None:
-            self.gpt2 = gpt2
-            self.distilgpt2 = distilgpt2
-
-    class WandbConfig:
-        """Specify the parameters of `wandb.init`"""
-
-        project: str = "project"
-        entity: str = "matteoperiani"
-        
-    dataset: Dataset = Dataset()
-    model_params: ModelParameters = ModelParameters()
-    checkpoints: Checkpoints = Checkpoints()
-    utils: Utils = Utils()
-    wandbConfig:WandbConfig = WandbConfig()
-
-    hp = PropertyDict(
-        seed=42,
-        checkpoint_name="distilgpt2",
-        model_name="distilgpt2",
-        padding_side="left",
-        train_perc=0.1,
-        val_perc=0.1,
-        batch_size=8,
-        num_epochs=1,
-        learning_rate=1e-4,
-        loss_alpha = 0.5,
-        scheduler="linear",
-        warmup_fraction=0.1,
-        accumulation_steps=1,
-        gradient_clip = 1.0,
-        mixed_precision="fp16",
-        log_interval=200,
-        eval_interval = 100000,
-        cpu=False
-    )
-
-
-CONFIG:Config = Config()
+        return config
